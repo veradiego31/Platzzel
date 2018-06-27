64729a456a6cc6af149b7cf096d8edad059a1366
veradiego31 <veradiego31@gmail.com>
2018-06-27 16:52:56 -0400

    remove last tile

--- Platzzel/ViewController.swift
+++ Platzzel/ViewController.swift
@@ -12,28 +12,62 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var board: UIView!
     var tileWidth: Int = 0
+    var tileCenterX: Int = 0
+    var tileCenterY: Int = 0
+    
+    var tileCenterArray : NSMutableArray = []
+    var tileArray : NSMutableArray = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-       let boardWidth = Int(self.board.frame.width)
+       makeTiles()
         
-        tileWidth = boardWidth / 4
         
-        for i in 0..<4 {
-            let x = i * tileWidth
-            for j in 0..<4 {
-                let y = j * tileWidth
-                let rect = CGRect(x: x, y: y, width: tileWidth - 2, height: tileWidth - 2)
+    }
+    
+    func makeTiles(){
+        let boardWidth = Int(self.board.frame.width)
+        self.tileWidth = boardWidth / 4
+        self.tileCenterX = tileWidth / 2
+        self.tileCenterY = tileWidth / 2
+        var tileNumber = 0
+        
+        for _ in 0..<4 {
+            
+            for _ in 0..<4 {
+                let rect = CGRect(x: 0, y: 0, width: tileWidth, height: tileWidth)
                 let tile = UILabel(frame: rect)
                 tile.backgroundColor = UIColor.red
+                let currentCenter = CGPoint(x: self.tileCenterX, y: tileCenterY)
+                tile.center = currentCenter
+                
+                self.tileCenterArray.add(currentCenter)
+                
                 self.board.addSubview(tile)
+                
+                tileNumber += 1
+                tile.text = "\(tileNumber)"
+                
+                self.tileArray.add(tile)
+                
+                self.tileCenterX += self.tileWidth
             }
+            
+            self.tileCenterX = self.tileWidth / 2
+            self.tileCenterY += self.tileWidth
             
         }
         
         
+        
+        let lastTile = self.tileArray.lastObject as! UILabel
+        
+        lastTile.removeFromSuperview()
+        
+        self.tileCenterArray.removeObject(at: 15)
+        self.tileArray.removeObject(at: 15)
     }
 
    
