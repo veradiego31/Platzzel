6c4ebf3841e88b57dbf1384b473ca012eccede1f
veradiego31 <veradiego31@gmail.com>
2018-06-26 21:12:03 -0400

    add board with tile

--- Platzzel/ViewController.swift
+++ Platzzel/ViewController.swift
@@ -10,15 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var board: UIView!
+    var tileWidth: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+       let boardWidth = Int(self.board.frame.width)
+        
+        tileWidth = boardWidth / 4
+        
+        for i in 0..<4 {
+            let x = i * tileWidth
+            for j in 0..<4 {
+                let y = j * tileWidth
+                let rect = CGRect(x: x, y: y, width: tileWidth - 2, height: tileWidth - 2)
+                let tile = UILabel(frame: rect)
+                tile.backgroundColor = UIColor.red
+                self.board.addSubview(tile)
+            }
+            
+        }
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+   
 
 
 }
